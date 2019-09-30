8a4291e3c7e23c65e397e2978fa0e6addfc304fe
Pelle <pelle.nieuw@gmail.com>
2019-09-30 11:24:19 +0200

    Database connectie

--- Startup.cs
+++ Startup.cs
@@ -9,6 +9,9 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using zorgapp.Models;
+
 
 namespace zorgapp
 {
@@ -31,8 +34,8 @@ namespace zorgapp
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
-
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddDbContext<DatabaseContext>(options => options.UseNpgsql(Configuration.GetConnectionString("DatabaseContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
