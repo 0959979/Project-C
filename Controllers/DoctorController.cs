ad41480fef94cb142aef352c8b35112c1e8b7d1e
ronanhub <45386974+ronanhub@users.noreply.github.com>
2020-01-03 22:48:41 +0100

    test naar admin en doctor controller

--- Controllers/DoctorController.cs
+++ Controllers/DoctorController.cs
@@ -957,5 +957,141 @@ namespace zorgapp.Controllers {
 
 			return View(Patientslist);
 		}
-	}
+
+        //TESTING
+        public DatabaseContext getContext()
+        {
+            return _context;
+        }
+        public IActionResult noAccess()
+        {
+            return View();
+        }
+        [Authorize(Roles = "Doctor")]
+        public IActionResult TestPage()
+        {
+            if (User.Claims.First(x => x.Type == ClaimTypes.NameIdentifier).Value.ToLower() != "admin")
+            {
+                return RedirectToAction("Login", "Patient");
+            }
+            List<Tuple<string, string>> tupleList = new List<Tuple<string, string>>();
+
+            List<DoctorTest> testlist = new List<DoctorTest>();
+            {
+                testlist.Add(new TemplateTest(this));
+            }
+            foreach (DoctorTest T in testlist)
+            {
+                tupleList.Add(new Tuple<string, string>(T.Id, T.Id));
+            }
+
+            TestListViewModel testlistmodel = new TestListViewModel { tuples = tupleList };
+            return View(testlistmodel);
+        }
+
+        public IActionResult StartTest(string TestId)
+        {
+            List<DoctorTest> testlist = new List<DoctorTest>();
+            {
+                testlist.Add(new TemplateTest(this));
+            }
+            DoctorTest testobj = testlist.FirstOrDefault();
+            foreach (DoctorTest T in testlist)
+            {
+                if (T.Id == TestId)
+                {
+                    testobj = T;
+                }
+            }
+            TestViewModel Model = testobj.Run();
+            return View(Model);
+        }
+    }
+    internal abstract class DoctorTest
+    {
+        public abstract TestViewModel Run();
+        public DoctorController testController;
+        public string Id;
+        public string Description;
+        public string Steps;
+        public string Criteria;
+        public string Inputstr;
+        public string Aresult;
+        public string Eresult;
+        public bool Pass;
+    }
+    internal class TemplateTest : DoctorTest
+    {
+        public TemplateTest(DoctorController tc)
+        {
+            testController = tc;
+            Id = ".Integration.";
+            Description = " test 1";
+            Steps = "Check if true == true";
+            Criteria = "true must be true";
+            Inputstr = "true";
+            Aresult = "";
+            Eresult = "True";
+        }
+
+        public override TestViewModel Run()
+        {
+            TestViewModel model;
+
+            //arrange
+            bool Pass = false;
+            DoctorController controller = testController;
+
+            //act
+            try
+            {
+                if (true == true)
+                {
+                    Aresult = "True";
+                }
+            }
+            catch (Exception e)
+            {
+                Pass = false;
+                Aresult = e.ToString();
+                model = new TestViewModel()
+                {
+                    id = Id,
+                    time = DateTime.Now,
+                    description = Description,
+                    steps = Steps,
+                    criteria = Criteria,
+                    input = Inputstr,
+                    aresult = Aresult,
+                    eresult = Eresult,
+                    pass = Pass
+                };
+                return model;
+            }
+
+            //assert
+            if (Aresult == Eresult)
+            {
+                Pass = true;
+            }
+            else
+            {
+                Pass = false;
+            }
+
+            model = new TestViewModel()
+            {
+                id = Id,
+                time = DateTime.Now,
+                description = Description,
+                steps = Steps,
+                criteria = Criteria,
+                input = Inputstr,
+                aresult = Aresult,
+                eresult = Eresult,
+                pass = Pass
+            };
+            return model;
+        }
+    }
 }
